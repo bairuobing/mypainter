83a8777e1f7cccdc7f2f856cae6a20bb9841855e
bairuobing <brb19960117@gmail.com>
2018-10-27 15:26:29 +0800

    对画板数据的边界限制以及客户端操作频率限制

--- src/app.js
+++ src/app.js
@@ -8,12 +8,15 @@ const port = 9095
 const app = express()
 const server = http.createServer(app)
 const wss = new ws.Server({server})
+
+const width = 70
+const height = 70
 // require就可以直接读取文件内的内容并 parse
 let pixelData
 try {
     pixelData = require('./pixel.json')
 } catch (error) {
-    pixelData = new Array(70).fill(0).map(it => new Array(70).fill('white'))
+    pixelData = new Array(height).fill(0).map(it => new Array(width).fill('white'))
 }
 
 // 不断的将数据以文件的形式保存下来记录（服务器挂掉不会影响数据）
@@ -29,19 +32,30 @@ wss.on('connection', (ws, req) => {
         type: 'init'
     }))
     // 当服务器把数据发送过去以后立马再监听客户端 的消息
+    // 每一次链接中不应该使得消息无间断发送，指定一个时间间隔
+    let lastDraw = 0 // 最后一次戳点时间戳
     ws.on('message', msg => {
         msg = JSON.parse(msg)
-        if(msg.type == 'drawDot'){
-            pixelData[msg.y][msg.x] = msg.color
-            // wss clients 对所有客户端发送更新
-            wss.clients.forEach(client => {
-                client.send(JSON.stringify({
-                    type: 'updateDot',
-                    x: msg.x,
-                    y: msg.y,
-                    color: msg.color
-                }))
-            })
+        let now = Date.now()
+        // 如果一次连接中的两次消息间隔小于 200ms 则不允许再次发送
+        if (now - lastDraw < 200) {
+            return
+        }
+        // 不能超出画板长宽范围
+        if (msg.x > 0 && msg.y > 0 && msg.x < width && msg.y < height) {
+            lastDraw = now // 更新lastDraw
+            if(msg.type == 'drawDot'){
+                pixelData[msg.y][msg.x] = msg.color
+                // wss clients 对所有客户端发送更新
+                wss.clients.forEach(client => {
+                    client.send(JSON.stringify({
+                        type: 'updateDot',
+                        x: msg.x,
+                        y: msg.y,
+                        color: msg.color
+                    }))
+                })
+            }
         }
     }
 )})
